cef65b8679aeaf9288f171790248afed86c5f25b
Kash-2g <162958950+Kash-2g@users.noreply.github.com>
2024-05-11 19:46:00 +0300

    first commit

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,18 +4,18 @@ function Item({ name, category }) {
   const [isInCart, setIsInCart] = useState(false);
 
   function handleAddToCartClick() {
-    setIsInCart((isInCart) => !isInCart);
+    setIsInCart((isInCart) =>!isInCart);
   }
 
   return (
-    <li className={isInCart ? "in-cart" : ""}>
+    <li className={isInCart? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
       <button
-        className={isInCart ? "remove" : "add"}
+        className={isInCart? "remove" : "add"}
         onClick={handleAddToCartClick}
       >
-        {isInCart ? "Remove From" : "Add to"} Cart
+        {isInCart? "Remove From" : "Add to"} Cart
       </button>
     </li>
   );
